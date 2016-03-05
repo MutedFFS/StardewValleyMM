fc3047aeb802d4ad09953d4fe4c41c387905859e
Yuuki <k.wrasee@gmail.com>
2016-03-05 17:49:57 +0100

    WIP

--- Class1.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-    Public Sub Main()
-
-        MsgBox("Hi", MsgBoxStyle.Information)
-        Application.EnableVisualStyles()
-        Application.SetCompatibleTextRenderingDefault(False)
-        Application.Run(New Form1)
-    End Sub
